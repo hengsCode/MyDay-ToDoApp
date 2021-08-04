e978f1c441a22a0c760b6ed962707d2c778bc721
hengsCode <61573350+hengsCode@users.noreply.github.com>
2021-08-04 20:32:23 +1000

    Drag and drop semi-functional

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,11 +1,5 @@
 import { React } from "react";
-import {
-  Card,
-  CardContent,
-  CardActionArea,
-  Button,
-  Radio,
-} from "@material-ui/core";
+import { Radio } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
 import "./styles.css";
 
@@ -18,26 +12,25 @@ const Task = (props) => {
   };
 
   return (
-    <div className="task-container">
-      <Card className="task-card">
-        <Radio
-          className="task-radio"
-          checked={task.checked}
-          onClick={handleRadio}
-          disableRipple
-          disableTouchRipple
-        />
-        <CardActionArea>
-          <CardContent className={taskCardContent}>{task.label}</CardContent>
-        </CardActionArea>
-        <Button
+    <div className="draggable" draggable>
+      <div className="task-card">
+        <div className="task-left">
+          <Radio
+            className="task-radio"
+            checked={task.checked}
+            onClick={handleRadio}
+            disableRipple
+            disableTouchRipple
+          />
+          <div className={taskCardContent}>{task.label}</div>
+        </div>
+        <Delete
+          className="task-delete-icon"
           onClick={() => {
             handleDelete(task.label);
           }}
-        >
-          <Delete className="task-delete-icon" />
-        </Button>
-      </Card>
+        />
+      </div>
     </div>
   );
 };
